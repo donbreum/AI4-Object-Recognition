51d4a960dba3c32aa637aa1a610ee5449f1a78ba
Per <lillep3r@hotmail.com>
2018-11-02 12:05:35 +0100

    add get object with all files and boxes

--- intersection_over_union/get_ground_truth.py
+++ intersection_over_union/get_ground_truth.py
@@ -11,12 +11,21 @@ import glob
 from pathlib import Path
 import os
 
-class box():
+class AllAnnotations():
     def __init__(self):
-        xmin = np.asarray([],dtype=np.int32)
-        ymin = np.asarray([],dtype=np.int32)
-        xmax = np.asarray([],dtype=np.int32)
-        ymax = np.asarray([],dtype=np.int32)
+        self.files = np.array([],dtype=np.ndarray)
+
+class AnnotationFile():
+    def __init__(self, name):
+        self.file_name = name
+        self.boxes = np.array([],dtype=np.ndarray)
+
+class Box():
+    def __init__(self):
+        self.xmin = np.asarray([],dtype=np.int32)
+        self.ymin = np.asarray([],dtype=np.int32)
+        self.xmax = np.asarray([],dtype=np.int32)
+        self.ymax = np.asarray([],dtype=np.int32)
 
 def init():
     annotation_path = sys.argv[1]
@@ -29,12 +38,15 @@ def init():
     print("End file path: {0}".format(end_file))
     return annotation_path, start_file, end_file
 
-def load_annotations(annotation_folder, start, end):
+def load_annotations(annotation_folder, start, end, file_collection):
     boxes = np.array(["",0,0,0,0],dtype=np.ndarray)
+    # files = np.array([],dtype=np.ndarray)
+    
     for infile in sorted(glob.glob(annotation_folder+ '*.xml')):
         file_name = os.path.basename(infile)
-        # print(file_name)
         
+        annotation_file = AnnotationFile(file_name)
+        # print(file_name)
         tree = ET.parse(annotation_folder + file_name)  
         root = tree.getroot()
         xmin = np.asarray([],dtype=np.int32)
@@ -57,18 +69,29 @@ def load_annotations(annotation_folder, start, end):
             boxes_file = np.array([filename_str, xmin,ymin,xmax,ymax])
             boxes_file_t = boxes_file.transpose()
             for i in range(len(boxes_file_t)):
-                boxes = np.vstack((boxes, boxes_file_t[i]))       
+                boxes = np.vstack((boxes, boxes_file_t[i]))   
+                box = Box()
+                box.xmin = boxes_file_t[i][1]
+                box.ymin = boxes_file_t[i][2]
+                box.xmax = boxes_file_t[i][3]
+                box.ymax = boxes_file_t[i][4]   
+                annotation_file.boxes = np.append(annotation_file.boxes,box)
+            file_collection.files = np.append(file_collection.files, annotation_file)
+            print("files append")
         else:
             print("No annotations found in %s" % file_name)
         
+    import pdb; pdb.set_trace()
     boxes = boxes[1:]
     print (boxes)
     
-    print("finish load annotation")
+    print("finish load annotations")
 
-def run():
+def run_get_ground_truth():
+    file_collection = AllAnnotations()
     a_path, start_file, end_file = init()
-    load_annotations(a_path, start_file, end_file)
+    load_annotations(a_path, start_file, end_file, file_collection)
+    
 
 if __name__=='__main__':
-    run()
+    run_get_ground_truth()
