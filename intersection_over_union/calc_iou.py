bb9b7953532fd1d22c9fb8e2a8eb7a2cc7b07d39
Per <lillep3r@hotmail.com>
2018-11-07 15:13:59 +0100

    working

--- intersection_over_union/calc_iou.py
+++ intersection_over_union/calc_iou.py
@@ -1,24 +1,25 @@
+import numpy as np
 # from https://www.pyimagesearch.com/2016/11/07/intersection-over-union-iou-for-object-detection/
-def bb_intersection_over_union(boxA, boxB):
-	
+def bb_intersection_over_union(truth, pred):
+
 	# determine the (x, y)-coordinates of the intersection rectangle
-	xA = max(boxA[0], boxB[0])
-	yA = max(boxA[1], boxB[1])
-	xB = min(boxA[2], boxB[2])
-	yB = min(boxA[3], boxB[3])
+	xA = max(truth.xmin, pred.xmin)
+	yA = max(truth.ymin, pred.ymin)
+	xB = min(truth.xmax, pred.xmax)
+	yB = min(truth.ymax, pred.ymax)
 
 	# compute the area of intersection rectangle
 	interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
 
 	# compute the area of both the prediction and ground-truth
 	# rectangles
-	boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
-	boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+	boxAArea = (truth.xmax - truth.xmin + 1) * (truth.ymax - truth.ymin + 1)
+	boxBArea = (pred.xmax - pred.xmin + 1) * (pred.ymax - pred.ymin + 1)
 
 	# compute the intersection over union by taking the intersection
 	# area and dividing it by the sum of prediction + ground-truth
 	# areas - the interesection area
 	iou = interArea / float(boxAArea + boxBArea - interArea)
-
+	# import pdb; pdb.set_trace()
 	# return the intersection over union value
 	return iou
