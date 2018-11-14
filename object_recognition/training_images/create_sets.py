b2ac01827d340ac313970f281023e4ebde6502e0
Kristian Hansen <kmolha@gmail.com>
2018-11-14 09:15:14 +0100

    Fix script that create sets so it gives relative paths

--- object_recognition/training_images/create_sets.py
+++ object_recognition/training_images/create_sets.py
@@ -3,23 +3,36 @@ import glob, os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 print(current_dir)
 # Percentage of images to be used for the test set
-percentage_test = 10;
+percentage_test = 10
 # Create and/or truncate train.txt and test.txt
-file_train = open('train.txt', 'w')  
-file_test = open('test.txt', 'w')
+file_train = open('../../darknet/train.txt', 'w')  
+file_test = open('../../darknet/test.txt', 'w')
 
 # txt_folder = "/source_image_annotation/27092018_pallet_moving_L_annotations_txt/"
-txt_folder = "/home/smatgaflen/Documents/github/AI4-Object-Recognition/object_recognition/training_images/source_image_annotation/27092018_pallet_moving_L_annotations_txt"
-img_path = "/home/smatgaflen/Documents/darknet/data/rockwool/images/downsampled"
+dji_path = current_dir + "/source_images/dji/samlet/"
+static_path = current_dir + "/source_images/static/"
+
+darknet_rel_path_dji = "../object_recognition/training_images/source_images/dji/samlet/"
+darknet_rel_path_static = "../object_recognition/training_images/source_images/static/"
 # Populate train.txt and test.txt
 counter = 1  
 index_test = round(100 / percentage_test)  
 # for pathAndFilename in glob.iglob(os.path.join(current_dir + txt_folder, "*.txt")):  
-for pathAndFilename in glob.iglob(os.path.join(txt_folder, "*.txt")): 
+for pathAndFilename in glob.iglob(os.path.join(dji_path, "*.jpg")): 
     title, ext = os.path.splitext(os.path.basename(pathAndFilename))
     if counter == index_test:
         counter = 1
-        file_test.write(img_path + "/" + title + '.jpg' + "\n")
+        file_test.write(darknet_rel_path_dji + title + '.jpg' + "\n")
     else:
-        file_train.write(img_path + "/" + title + '.jpg' + "\n")
+        file_train.write(darknet_rel_path_dji + title + '.jpg' + "\n")
         counter = counter + 1
+
+for pathAndFilename in glob.iglob(os.path.join(static_path, "*.txt")): 
+    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+    if counter == index_test:
+        counter = 1
+        file_test.write(darknet_rel_path_static + title + '.jpg' + "\n")
+    else:
+        file_train.write(darknet_rel_path_static + title + '.jpg' + "\n")
+        counter = counter + 1
+        
