88ea17363620bc498077188d192d50288773d01a
Kristian Hansen <kmolha@gmail.com>
2018-11-21 15:07:07 +0100

    Finish training script to put all outputs in the same place

--- darknet/scripts/train_rockwool.sh
+++ darknet/scripts/train_rockwool.sh
@@ -12,9 +12,6 @@ while [ "$1" != "" ]; do
         -w | --weights )		shift
 								weights=$1
 								;;
-        -s | --structure )		shift
-								structure=$1
-								;;
         * )                     
     esac
     shift
@@ -23,10 +20,11 @@ done
 cd ..
 
 #../darknet detector train ../cfg/rockwool.data ../cfg/rockwool-yolov3-tiny.cfg ../rockwool-yolov3-tiny_1500.weights
-./darknet detector train cfg/$datafile cfg/$cfgfile $weights -dont_show
+#./darknet detector train cfg/$datafile cfg/$cfgfile $weights -dont_show
 
 weights_folder=backup
-final_weights_folder=final_weights
+final_results_folder=final_results
+model="${cfgfile%.*}"
 
 if [ -e results.txt ]; then
 	rm results.txt
@@ -47,23 +45,28 @@ if [ -e thresholds.txt ]; then
 	rm thresholds.txt
 fi
 
-if [ ! -d final_weights ]; then
-	mkdir final_weights
+if [ ! -d $final_results_folder ]; then
+	mkdir $final_results_folder
 fi
 
 # find weights file with lowest recall score and return filename
-name=$(python3 scripts/find_best_weight_file.py backup/ results.txt $structure final_weights_folder)
+name=$(python3 scripts/find_best_weight_file.py backup/ results.txt $model final_results_folder)
 best_weight=$(date +%y%m%d)_$name
 no_extension="${best_weight%.*}"
 
-cp $weights_folder/$name $final_weights_folder/$best_weight
+cp $weights_folder/$name $final_results_folder/$best_weight
 
-echo "Moving to "$final_weights_folder/$no_extension"_results.txt"
-mv results.txt $final_weights_folder/$no_extension"_results.txt"
+echo "Moving results to "$final_results_folder/$no_extension"_results.txt"
+mv results.txt $final_results_folder/$no_extension"_results.txt"
 
 for thresh_val in $(seq 0.5 0.01 0.99); do	
-	./darknet detector map cfg/$datafile cfg/$cfgfile $final_weights_folder/$best_weight -thresh $thresh_val
+	./darknet detector map cfg/$datafile cfg/$cfgfile $final_results_folder/$best_weight -thresh $thresh_val
 done
 
-echo "Moving to "$final_weights_folder/$no_extension"_thresholds.txt"
-mv thresholds.txt $final_weights_folder/$no_extension"_thresholds.txt"
+echo "Moving thresholds to "$final_results_folder/$no_extension"_thresholds.txt"
+mv thresholds.txt $final_results_folder/$no_extension"_thresholds.txt"
+
+python3 scripts/plot_threshold_results.py $final_results_folder/$no_extension"_thresholds.txt" $model
+
+echo "Exporting graph to "$final_results_folder/$(date +%y%m%d)_$model"_recall_vs_iou.png"
+mv $model"_recall_vs_iou.png" $final_results_folder/$(date +%y%m%d)_$model"_recall_vs_iou.png"
