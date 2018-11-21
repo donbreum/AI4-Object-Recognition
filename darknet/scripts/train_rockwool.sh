078fb625684e5459b7e47b71f2f49c36cf93f19e
Kristian Hansen <kmolha@gmail.com>
2018-11-21 11:08:58 +0100

    Finish script for recording results and training

--- darknet/scripts/train_rockwool.sh
+++ darknet/scripts/train_rockwool.sh
@@ -23,7 +23,7 @@ done
 cd ..
 
 #../darknet detector train ../cfg/rockwool.data ../cfg/rockwool-yolov3-tiny.cfg ../rockwool-yolov3-tiny_1500.weights
-#./darknet detector train cfg/$datafile cfg/$cfgfile $weights -dont_show
+./darknet detector train cfg/$datafile cfg/$cfgfile $weights -dont_show
 
 weights_folder=backup
 final_weights_folder=final_weights
@@ -38,9 +38,6 @@ for filename in $weights_folder/*.weights; do
 	filename=$(basename $filename)
 	
 	./darknet detector map cfg/$datafile cfg/$cfgfile backup/$filename
-	dummy=$filename
-	
-	echo $dummy
 
 	python3 scripts/append_name.py $filename results.txt 
 done
@@ -54,13 +51,19 @@ if [ ! -d final_weights ]; then
 	mkdir final_weights
 fi
 
+# find weights file with lowest recall score and return filename
 name=$(python3 scripts/find_best_weight_file.py backup/ results.txt $structure final_weights_folder)
-# find weights file with lowest recall score
-
-cp $weights_folder/$name $final_weights_folder/$(date +%y%m%d)_$name
-
 best_weight=$(date +%y%m%d)_$name
+no_extension="${best_weight%.*}"
+
+cp $weights_folder/$name $final_weights_folder/$best_weight
+
+echo "Moving to "$final_weights_folder/$no_extension"_results.txt"
+mv results.txt $final_weights_folder/$no_extension"_results.txt"
 
 for thresh_val in $(seq 0.5 0.01 0.99); do	
 	./darknet detector map cfg/$datafile cfg/$cfgfile $final_weights_folder/$best_weight -thresh $thresh_val
 done
+
+echo "Moving to "$final_weights_folder/$no_extension"_thresholds.txt"
+mv thresholds.txt $final_weights_folder/$no_extension"_thresholds.txt"
